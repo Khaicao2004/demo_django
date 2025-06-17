b50228ae99be22e9a894e945ad78057379cfb143
khaicq <khaicq@newit.co.jp>
2025-06-17 17:40:03 +0700

    Add new fields to users

--- app/demo_django/views.py
+++ app/demo_django/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Users
+from django.contrib import messages
+
 
 # Create your views here.
 def index_users(request):
@@ -14,9 +16,13 @@ def store_users(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
-        password = request.POST.get('password')
-        Users.objects.create(name=name, email=email, password=password)
-        return redirect('users.index')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        Users.objects.create(name=name, email=email, address=address, phone=phone)
+        messages.success(request, 'Lưu bản ghi thành công')
+    else:
+        messages.error(request, 'Lưu bản ghi thất bại')
+    return redirect('users.index')
 
 def edit_users(request, id): 
     user = Users.objects.get(id=id)
@@ -28,10 +34,18 @@ def update_users(request, id):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
-        Users.objects.get(id=id).update(name=name,email=email)
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        Users.objects.filter(id=id).update(name=name,email=email, address=address, phone=phone)
+        messages.success(request, 'Cập nhật bản ghi thành công')
+    else:
+        messages.error(request, 'Cập nhật bản ghi thất bại')
     return redirect('users.index')
 
 def delete_users(request, id):
     if request.method == 'POST':
-        Users.objects.filter(id=id).delete()
-        return redirect('users.index')
+        Users.objects.get(id=id).delete()
+        messages.success(request, 'Xoá bản ghi thành công')
+    else:
+        messages.error(request, 'Xoá bản ghi thất bại')
+    return redirect('users.index')
