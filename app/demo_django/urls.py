27f5b16de4844db2187e9536064575f0326a8d55
khaicq <khaicao2004@gmail.com>
2025-06-19 22:56:48 +0700

    Add related models

--- app/demo_django/urls.py
+++ app/demo_django/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('users/', views.index_users, name='users.index'),
     path('users/create/', views.create_users, name='users.create'),
     path('users/store/', views.store_users, name='users.store'),
-    path('users/edit/<str:id>', views.edit_users, name='users.edit'),
-    path('users/update/<str:id>', views.update_users, name='users.update'),
-    path('users/delete/<str:id>', views.delete_users, name='users.delete'),
+    path('users/edit/<str:id>/', views.edit_users, name='users.edit'),
+    path('users/update/<str:id>/', views.update_users, name='users.update'),
+    path('users/delete/<str:id>/', views.delete_users, name='users.delete'),
     path('books/', views.index_books, name='books.index'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
